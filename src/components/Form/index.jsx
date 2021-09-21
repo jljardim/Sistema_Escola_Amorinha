8d3a5f4365dcf00fb33dd2d731e699bd32a0929a
Jeferson Jardim <jeferson_luiz@hotmail.com>
2021-09-21 00:45:33 -0300

    checando update

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,41 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
 
-class Form extends React.Component {
+
+const Form = (props) => {
+
+  const [name, setName] = useState('');
+  const [date, setDate] = useState('');
+  const [avisar, setAvisar] = useState(['Pai', 'Mãe', 'Tio', 'Tia', 'Avós'])
+  const [selectedPosition, setSelectedPosition] = useState('')
+/* class Form extends React.Component {
    constructor(props){
     super(props);
     this.state={
         name: "",
         date: "",
         avisar: ["Pai", "Mãe", "Tio", "Tia", "Avós"],
+        selectedPosition: "",
 
 
   };
- };
- componentDidMount () {
+ }; */
+
+ const handleChange = (event) => {
+  const inputName = event.target.name;
+  const value = event.target.value;
+  if (inputName === "phone") {
+    this.setPhone(value);
+  } else {
+    this.setState({ [inputName]: event.target.value });
+  }
+};
+
+  const onSubmit = (event) => {
+    console.log({event});
+    event.preventDefault();
+
+  }
+
+/*  componentDidMount () {
   const posicao = this.props.avisar;
   this.setState({
     posicao,
   });
- }
-    render () {
+ } */
+
       return(
-          <>
-        <form>
+          
+        <form onSubmit={onSubmit}>
 
          <TextField 
-            label={this.props.lblName}
+            label={props.lblName}
             type="text"
             name="name"
             htmlFor="name"
-            value={this.props.name}
-            placeholder={this.props.plch_name}
+            value={props.name}
+            placeholder={props.plch_name}
             required
             fullWidth
           />
 
           <TextField 
-            label={this.props.lblDate}
+            label={props.lblDate}
             type="date"
             name="date"
             fullWidth
@@ -43,59 +69,65 @@ class Form extends React.Component {
           />
 
           <TextField 
-             label={this.props.lblResponsavel}
+             label={props.lblResponsavel}
              type="text"
              name="responsabel"
              htmlFor="responsavel"
-             value={this.props.responsavel}
-             placeholder={this.props.plch_responsavel}
+             value={props.responsavel}
+             placeholder={props.plch_responsavel}
              required
              fullWidth
-          
-          />
+              />
+        
           <TextField 
                label="Telefone"
                type="tel"
                name="telefone"
                htmlFor="telefone"
-               value={this.props.telefone}
+               value={props.telefone}
                required
                fullWidth
           
           />
-          <TextField 
-              label={this.props.lblEmergencia}
+          <TextField
+              label={props.lblEmergencia}
               select
-              value={this.state.posicao} 
-              name="emergencia" 
+              value={selectedPosition} 
+              onChange={handleChange}
+              name="selectedPosition" 
               fullWidth
-          />
+          >
+            {avisar.map((item, index) => {
+              console.log(item);
+              return (
+                <MenuItem key={index} value={item}>
+                  {item}
+                </MenuItem>
+              );
+            })}
+          </TextField >
           <TextField 
             type="checkbox"
             text="Possui alguma restição alimentar?"
             required
           />
-
-                    {/* <input type="checkbox" required/>Possui alguma restição alimentar? */}
-                    <br/>
-                    <label for="restri">{this.props.restricoes}</label>
+                    <label for="restri">{props.restricoes}</label>
                     <input type="text" id="restri" disabled placeholder="Informe quais restrições a criança tem" />
                     <br/>
                     <input type="checkbox" required/>Autorizo o uso de imagems e videos para fins escolar?
                     <br/>
-                    <select>{this.props.lista}
+                    <select>{props.lista}
                         <option value="">Turma</option>
                         <optgroup></optgroup>
                     </select>
                     <br/><br/>
-                    <textarea>{this.props.observacoes}</textarea>
+                    <textarea>{props.observacoes}</textarea>
                     <br/>
                     <input type="submit"/>
                {/*  </fieldset> */}
             </form>
-            </>
+            
         );
-    }
 }
 
 export default Form;
